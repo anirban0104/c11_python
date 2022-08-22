5a80bf22d41e9e2f59c2e5c0afcefa7aec27ba51
anirban0104 <anirban0104@gmail.com>
2022-08-22 12:25:11 +0530

    Deleted a sussy file

--- personalTests/gayChecker.py
+++
@@ -1,5 +0,0 @@
-gender=str(input("Do you suck cock? "))
-if gender=='Yes' or 'yes' or 'Y' or 'y' or 'ya' or 'Ya' or 'Yep' or 'ys' or 'Ys' or 'YES':
-    print("You are fucking gay bro, stay away")
-else:
-    print("You are straight like my bichi, dw")
